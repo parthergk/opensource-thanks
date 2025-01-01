4187a345c231132f653538769f96e5e81a765080
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-01 23:11:30 +0530

    get-the-commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+// import { useState } from "react";
 
 
 function App() {
+  // const [contributors, setContributors] = useState("");
+
+   async function getContributors(){
+    const respose = await fetch("https://api.github.com/repos/hiteshchoudhary/apihub/commits");
+    const result = await respose.json();
+    console.log("All contributors", result);
+  }
+
+  getContributors();
+
 
   return (
     <main className="bg-neutral-700 text-white w-full h-screen">
-      Hello
+      Thank Contributors
     </main>
   )
 }
